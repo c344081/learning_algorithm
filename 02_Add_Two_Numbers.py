7e1470739d77fc2a08e40bf20f55be99e9246de7
c344081 <c344081@gmail.com>
2017-08-31 23:41:37 +0800

    add_two_numbers

--- 02_Add_Two_Numbers.py
+++ 02_Add_Two_Numbers.py
@@ -9,10 +9,10 @@ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 '''
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
@@ -21,3 +21,46 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
+        lastNode = None
+        head = None
+        while(l1 or l2):
+            if not l1:
+                value = l2.val
+            elif not l2:
+                value = l1.val
+            else:
+                value  = l1.val + l2.val
+            if not lastNode:
+                lastNode = ListNode(value)
+                head = lastNode
+            else:
+                if lastNode.val >= 10:
+                    lastNode.val -= 10
+                    value += 1
+                lastNode.next = ListNode(value)
+                lastNode = lastNode.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+            if not l1 and not l2:
+                if value >= 10:
+                    lastNode.val -= 10
+                    lastNode.next = ListNode(1)
+        return head
+
+l1 = ListNode(2)
+# l1.next = ListNode(4)
+# l1.next.next = ListNode(3)
+
+l2 = ListNode(0)
+# l2.next = ListNode(6)
+# l2.next.next = ListNode(4)
+
+solution = Solution()
+l3 = solution.addTwoNumbers(l1, l2)
+
+l = l3
+while l:
+    print("val:%d" % l.val)
+    l = l.next
