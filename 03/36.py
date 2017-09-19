ec3b662f944cbcd868bf17089fd3c1a5d9e7f35a
c344081 <c344081@gmail.com>
2017-09-19 17:27:56 +0800

    n queens

--- 03/36.py
+++ 03/36.py
@@ -19,7 +19,24 @@ class Solution(object):
         :rtype: bool
         """
         #
-        for y in range(len(board)):
+        if not board:
+            return True
+
+        row = [[] for _ in range(9)]
+        col = [[] for _ in range(9)]
+        subGrid = [[] for _ in range(9)]
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                cell = board[i][j]
+                if cell != ".":
+                    leftTop = i / 3 * 3 + j / 3
+                    if cell in row[i] or cell in col[j] or cell in subGrid[leftTop]:
+                        return False
+                    row[i].append(cell)
+                    col[j].append(cell)
+                    subGrid[leftTop].append(cell)
+        return True
 
 
 
--- 03/542.py
+++ 03/542.py
@@ -30,11 +30,63 @@ The number of elements of the given matrix will not exceed 10,000.
 There are at least one 0 in the given matrix.
 The cells are adjacent in only four directions: up, down, left and right.
 """
+
+from collections import deque
+import sys
 class Solution(object):
     def updateMatrix(self, matrix):
         """
         :type matrix: List[List[int]]
         :rtype: List[List[int]]
         """
+        if not matrix:
+            return []
+        m, n = len(matrix), len(matrix[0])
+        queue = deque()
+
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    queue.append((i, j))
+                else:
+                    matrix[i][j] = sys.maxsize
+
+        while queue:
+            i, j = queue.popleft()
+            # 设置临近点
+            for dx, dy in zip((1, -1, 0, 0), (0, 0, 1, -1)):
+                ny = i + dy
+                nx = j + dx
+
+                if ny < 0 or ny >= m or nx < 0 or nx >= n:
+                    continue
+                value = matrix[ny][nx]
+                if value <= matrix[i][j] + 1: # 更新过, 且更小(近)
+                    continue
+                # 加入队列继续处理
+                queue.append((ny, nx))
+                # 更新, 临近点 + 1
+                matrix[ny][nx] = matrix[i][j] + 1
+        return matrix
+
+
+
+# matrix = [
+#     [0],
+#     [0],
+#     [0],
+#     [0],
+#     [0]
+# ]
+matrix = [
+    [0, 0, 0],
+    [0, 1, 0],
+    [1, 1, 1]
+]
+s = Solution()
+list = s.updateMatrix(matrix)
+print(list)
+
+
 
 
