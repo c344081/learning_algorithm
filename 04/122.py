953f0bfb95bb40be024740bdcf3c19f8684e39a8
c344081 <c344081@gmail.com>
2017-09-25 00:16:35 +0800

    Minimum Path Sum

---
+++ 04/122.py
@@ -0,0 +1,23 @@
+"""
+Best Time to Buy and Sell Stock II
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit.
+You may complete as many transactions as you like
+(ie, buy one and sell one share of the stock multiple times).
+However, you may not engage in multiple transactions at the same time
+(ie, you must sell the stock before you buy again).
+"""
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+
+prices = [7, 1, 5, 3, 6, 4, 8]
+s = Solution()
+profit = s.maxProfit(prices)
+print(profit)
